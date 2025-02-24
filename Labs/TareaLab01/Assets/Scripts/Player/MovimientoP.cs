5a114b1637441483fd9eb7d0d6a8a855b4e11cbb
Bruno Ortiz <bruno.ortiz_a10@ciencias.unam.mx>
2025-02-24 14:11:07 -0600

    Movimento de personaje con Character Controler

--- Labs/TareaLab01/Assets/Scripts/Player/MovimientoP.cs
+++ Labs/TareaLab01/Assets/Scripts/Player/MovimientoP.cs
@@ -1,26 +1,42 @@
+using System;
 using UnityEngine;
 
-public class MovimientoP : MonoBehaviour
+public class MovimientoPlayer : MonoBehaviour
 {
-    public float velocidad;
-    public float movVertical, movHorizontal;
-    private Rigidbody rb;
+    public float movHorizontal;
+    public float movVertical;
+    public float velocidad, velocidadOriginal;
+    public float gravedad;
+
+    public Vector3 direccion;
+    public CharacterController cc;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rb = gameObject.GetComponent<Rigidbody>();
+        cc = this.gameObject.GetComponent<CharacterController>();
+        velocidadOriginal = velocidad;
     }
 
     // Update is called once per frame
     void Update()
     {
-        movHorizontal = Input.GetAxis("Horizontal");
-        movVertical = Input.GetAxis("Vertical");
+        direccion = Vector3.zero;
+        direccion.y -= gravedad * Time.deltaTime;
+        direccion.x = Input.GetAxisRaw("Horizontal") * velocidad;
+        direccion.z = Input.GetAxisRaw("Vertical") * velocidad;
 
-        Vector3 movimiento = new Vector3(movHorizontal, 0, movVertical);
+        //this.transform.position += direccion*Time.deltaTime;
 
-        rb.AddForce(movimiento*velocidad);
+        cc.Move(direccion*Time.deltaTime);
+    }
+
+    public void setVelocidad(float velocidad) {
+        this.velocidad = velocidad;
+    }
+
+    public void resetVelocidad() {
+        this.velocidad = velocidadOriginal;
     }
 
     private void OnTriggerEnter(Collider other)
